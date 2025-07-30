d4e668a6fe1a77427c295d8ad09a13da0f322167
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-07-29 20:46:29 -0400

    Implement a private function to store contents @file_path

--- sum_functions.cpp
+++ sum_functions.cpp
@@ -1,10 +1,23 @@
 #include "sum_functions.h"
 
+#include <iostream>
 #include <string>
 #include <string_view>
+#include <fstream>
 
-Sum::SequentialSum() const {
-
+std::string Sum::_ReadFile() const {
+    ifstream file(Sum::file_path);
+    if (!file.is_open()) {
+        std::cout << "Unable to open " << Sum::file_path << "\n";
+        return "";
+    }
+    std::string file_contents;
+    std::string buffer;
+    while (std::getline(file, buffer)) {
+        file_contents += buffer;
+        file_contents.push_back('\n');
+    }
+    return file_contents;
 }
 
 Sum::MultiThreadedSum() const {
