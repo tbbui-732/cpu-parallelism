7eca60c7043946ae97429b0eedd2be751f3909e3
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-07-29 20:51:35 -0400

    Add comments describing _ReadFile

--- sum_functions.h
+++ sum_functions.h
@@ -5,7 +5,19 @@
 #include <string_view>
 
 struct Sum {
+
+    /*
+     * Read-only string of the file containing the array to be summed
+     */
     std::string_view file_path;
+
+    /*
+     * Stores content of @file_path in a std::string object
+     * Returns string content if SUCCESSFUL; otherwise an empty string ""
+     */
+    std::string _ReadFile() const;
+
+
     void SequentialSum() const;
     void MultiThreadedSum() const;
 };
