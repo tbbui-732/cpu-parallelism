2c6e6930f1c2821132293d42a0c166160609a017
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-08-08 15:05:25 -0400

    test source to vec function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include "sum_functions.h"
+#include <chrono>
 #include <iostream>
+#include <random>
+#include <string>
+
+#include "sum_functions.h"
+#include "test_sum_functions.h"
 
 int main() {
+    Test test;
+    test.TestSourceToVec();
     return 0;
 }
