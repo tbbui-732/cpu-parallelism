e8acf3fae7b3e0d45aabffdd5e4f9a4a1c10b436
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-08-08 15:05:54 -0400

    fix error related to fetching stream
    used stringstream instead of ifstream for files. wrote the remainder of
    the test functions

--- sum_functions.cpp
+++ sum_functions.cpp
@@ -4,11 +4,13 @@
 #include <string>
 #include <string_view>
 #include <fstream>
+#include <vector>
+#include <sstream>
 
-std::string Sum::_ReadFile() const {
-    ifstream file(Sum::file_path);
+std::string Sum::ReadFile(const std::string& file_path) {
+    std::ifstream file(file_path);
     if (!file.is_open()) {
-        std::cout << "Unable to open " << Sum::file_path << "\n";
+        std::cout << "Unable to open " << file_path << "\n";
         return "";
     }
     std::string file_contents;
@@ -17,10 +19,29 @@ std::string Sum::_ReadFile() const {
         file_contents += buffer;
         file_contents.push_back('\n');
     }
+    file.close();
     return file_contents;
 }
 
-Sum::MultiThreadedSum() const {
-
+std::vector<int> Sum::SourceToVec(const std::string& source) {
+    std::vector<int> result;
+    std::ifstream stream(source);
+    if (!stream) {
+        std::cout << "failed to initialize input file stream\n";
+        return result; // empty vector on failure
+    }
+    for (std::string buffer; std::getline(stream, buffer);) {
+        int conv = std::stoi(buffer);
+        result.push_back(conv);
+    }
+    stream.close();
+    return result;
 }
 
+void Sum::SequentialSum() {
+    return;
+}
+
+void Sum::MultiThreadedSum() {
+    return;
+}
