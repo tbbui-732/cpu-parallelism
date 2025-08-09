1b8757f8aaf757665b34fbd4f4284d117b962a09
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-08-08 22:18:09 -0400

    prepare for performance analysis

--- main.cpp
+++ main.cpp
@@ -5,13 +5,25 @@
 
 #include "sum_functions.h"
 #include "test_sum_functions.h"
+#include "utils.h"
 
 int main() {
-    Test test;
+    /* set up */
+    Utils::Setup();
+    std::vector<int> values = Sum::SourceToVec("test/arr.txt");
 
+    /* testing */
     std::cout << "test #1: SourceToVec - ";
-    test.TestSourceToVec();
+    Test::SourceToVec();
     std::cout << "passed\n";
+    std::cout << "\n";
+
+    /* performance analysis */
+    int total = Sum::SequentialSum(values);
+    std::cout << "total (sequential) - " << total << '\n';
+
+    total = Sum::MultiThreadedSum(values);
+    std::cout << "total (multithreaded) - " << total << '\n';
 
     return 0;
 }
