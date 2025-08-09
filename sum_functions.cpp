1c2543d99fc61b558fd0c327b81ed5de88584f12
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-08-08 22:18:40 -0400

    move functions over to utils_h

--- sum_functions.cpp
+++ sum_functions.cpp
@@ -7,22 +7,6 @@
 #include <vector>
 #include <sstream>
 
-std::string Sum::ReadFile(const std::string& file_path) {
-    std::ifstream file(file_path);
-    if (!file.is_open()) {
-        std::cout << "Unable to open " << file_path << "\n";
-        return "";
-    }
-    std::string file_contents;
-    std::string buffer;
-    while (std::getline(file, buffer)) {
-        file_contents += buffer;
-        file_contents.push_back('\n');
-    }
-    file.close();
-    return file_contents;
-}
-
 std::vector<int> Sum::SourceToVec(const std::string& source) {
     std::vector<int> result;
     std::ifstream stream(source);
@@ -38,10 +22,15 @@ std::vector<int> Sum::SourceToVec(const std::string& source) {
     return result;
 }
 
-void Sum::SequentialSum() {
-    return;
+// TODO: write this function, its header, and then test it
+int Sum::SequentialSum(const std::vector<int> values) {
+    int result = 0;
+    for (const int value : values) {
+        result += value;
+    }
+    return result;
 }
 
-void Sum::MultiThreadedSum() {
-    return;
+int Sum::MultiThreadedSum(const std::vector<int> values) {
+    return 0;
 }
